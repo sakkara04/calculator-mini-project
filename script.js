a6c69e5275e8cc581d208d3b3a9aefa710252e64
sara <ssriva16@ucsc.edu>
2024-11-05 15:19:51 -0800

    left side done

--- script.js
+++ script.js
@@ -2,60 +2,74 @@ let tipPer = 0;
 let totalPer = 0;
 let bill = 0;
 let tip = 0;
-let num = 0;
+let num = 1; 
 
 let resetButton = document.getElementById("reset");
 let tipPerAmt = document.getElementById("tip");
 let totalPerAmt = document.getElementById("total");
 let billAmt = document.getElementById("bill");
 let numAmt = document.getElementById("num");
+let tipButtons = document.querySelectorAll(".tip-buttons button");
 
 function toggleResetButton() {
-    if (tipPer > 0 || totalPer > 0) {
-      resetButton.disabled = false;  // Enable the button
-      resetButton.classList.remove("disabled");
-      resetButton.classList.add("enabled");
-    } else {
-      resetButton.disabled = true;  // Disable the button
-      resetButton.classList.remove("enabled");
-      resetButton.classList.add("disabled");
-    }
+  if (bill > 0 || tip > 0 || num > 1) {
+    resetButton.disabled = false;
+    resetButton.classList.remove("disabled");
+    resetButton.classList.add("enabled");
+  } else {
+    resetButton.disabled = true;
+    resetButton.classList.remove("enabled");
+    resetButton.classList.add("disabled");
   }
-
-toggleResetButton();
-
-resetButton.addEventListener("click", function() {
-    tipPer = 0;
-    totalPer = 0;
-    tipPerAmt.innerHTML = "$" + tipPer.toFixed(2);
-    totalPerAmt.innerHTML = "$" + totalPer.toFixed(2);
-    billAmt.value = "";
-    numAmt.value = "";
-    toggleResetButton(); 
-  });
-
-
-billAmt.addEventListener("keydown", function(event) {
-if (event.key === "Enter") {
-    bill = Number(billAmt.value);
-    tipPer = Number(billAmt.value)
-    tipPerAmt.innerHTML = "$" + tipPer.toFixed(2);
-    totalPerAmt.innerHTML = "$" + bill.toFixed(2);
-    toggleResetButton(); 
-    }
- });
-
- numAmt.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        num = numAmt.value;
-        if ((bill != 0) && (num != 0)){
-            calculatePer();
-        }
-    }
-     });
-
-function calculatePer(){
-    totalPer = bill / num;
-    totalPerAmt.innerHTML = "$" + totalPer.toFixed(2);
-    toggleResetButton(); 
 }
+
+resetButton.addEventListener("click", function () {
+  tipPer = 0;
+  totalPer = 0;
+  bill = 0;
+  tip = 0;
+  num = 1;
+  tipPerAmt.innerHTML = "$0.00";
+  totalPerAmt.innerHTML = "$0.00";
+  billAmt.value = "";
+  numAmt.value = "";
+  toggleResetButton();
+});
+
+function calculatePerPerson() {
+  if (num > 0) {
+    tipPer = (bill * tip) / 100 / num;
+    totalPer = (bill / num) + tipPer;
+    tipPerAmt.innerHTML = "$" + tipPer.toFixed(2);
+    totalPerAmt.innerHTML = "$" + totalPer.toFixed(2);
+    toggleResetButton();
+  }
+}
+
+tipButtons.forEach(button => {
+  button.addEventListener("click", function () {
+    tip = button.textContent.includes("%") ? parseInt(button.textContent) : 0;
+    calculatePerPerson();
+  });
+});
+
+billAmt.addEventListener("input", function () {
+  bill = parseFloat(billAmt.value) || 0;
+  calculatePerPerson();
+});
+
+numAmt.addEventListener("input", function () {
+  num = parseInt(numAmt.value) || 1; 
+  calculatePerPerson();
+});
+
+let customTipInput = document.createElement("input");
+customTipInput.setAttribute("type", "number");
+customTipInput.setAttribute("placeholder", "Custom");
+customTipInput.classList.add("custom-tip-input");
+tipButtons[5].replaceWith(customTipInput); 
+
+customTipInput.addEventListener("input", function () {
+  tip = parseFloat(customTipInput.value) || 0;
+  calculatePerPerson();
+});
